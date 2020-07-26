1e0b4a5f5a1f4b4683ce4af1ac11106964804a8f
Italo Leandro <leandroitalo433@gmail.com>
2020-07-26 19:19:04 -0300

    chore: change import Validation to login protocols controller

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,6 +1,5 @@
-import { Controller, HttpRequest, HttpResponse, Authentication } from './login-protocols-controller'
+import { Controller, HttpRequest, HttpResponse, Validation, Authentication } from './login-protocols-controller'
 import { badRequest, serverError, unauthorized, ok } from '../../helpers/http/http-helper'
-import { Validation } from '../signup/signup-protocols-controller'
 
 export class LoginController implements Controller {
   constructor (
