61313c349ec55ba1b9b40bfcf8ca4968db368d19
Italo Leandro <leandroitalo433@gmail.com>
2020-07-26 14:04:23 -0300

    test: ensure SignUpController returns an accessToken on success

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -104,7 +104,7 @@ describe('Signup Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest())
 
-    expect(httpResponse).toEqual(ok(makeFakeAccount()))
+    expect(httpResponse).toEqual(ok({ accessToken: 'any_token' }))
   })
 
   test('Should call Validation with correct values', async () => {
