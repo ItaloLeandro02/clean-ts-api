9669c6d6d898b3f30234f7b922bca755e37ab262
Italo Leandro <leandroitalo433@gmail.com>
2020-08-21 07:12:31 -0300

    refactor: move docs to separate files

---
+++ src/main/docs/schemas.ts
@@ -0,0 +1,25 @@
+import {
+  accountSchema,
+  loginParamsSchema,
+  errorSchema,
+  surveysSchema,
+  surveySchema,
+  surveyAnswerSchema,
+  signUpParamsSchema,
+  addSurveyParamsSchema,
+  saveSurveyParamsSchema,
+  surveyResultSchema
+} from './schemas/'
+
+export default {
+  account: accountSchema,
+  loginParams: loginParamsSchema,
+  error: errorSchema,
+  surveys: surveysSchema,
+  survey: surveySchema,
+  surveyAnswer: surveyAnswerSchema,
+  signUpParams: signUpParamsSchema,
+  addSurveyParams: addSurveyParamsSchema,
+  saveSurveyParams: saveSurveyParamsSchema,
+  surveyResult: surveyResultSchema
+}
